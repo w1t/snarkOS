158ebf081961d6bb41232d05ee2929fd10671914
Daniil Ryzhkov <daniil.re@gmail.com>
2020-06-11 00:54:34 +0300

    Attempt to add link-dead-code to rustc

---
+++ ci/setup-environment.sh
@@ -0,0 +1,7 @@
+export SCCACHE_CACHE_SIZE=200M
+export WORK_DIR="$CIRCLE_WORKING_DIRECTORY/.cache/sccache"
+export SCCACHE_DIR="$CIRCLE_WORKING_DIRECTORY/.cache/sccache"
+export PATH="$PATH:$CIRCLE_WORKING_DIRECTORY/.bin"
+export RUSTC_WRAPPER="sccache"
+export RUST_MIN_STACK=8388608
+export RUSTFLAGS="-Clink-dead-code"
