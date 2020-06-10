ba27b747ee9a234620aef84d1a746c7c287ffd20
Daniil Ryzhkov <daniil.re@gmail.com>
2020-06-11 02:22:35 +0300

    Fixed path definition

--- ci/setup-environment.sh
+++ ci/setup-environment.sh
@@ -1,7 +1,8 @@
+export WORK_DIR=/home/circleci/
 export SCCACHE_CACHE_SIZE=200M
-export WORK_DIR="$CIRCLE_WORKING_DIRECTORY/.cache/sccache"
-export SCCACHE_DIR="$CIRCLE_WORKING_DIRECTORY/.cache/sccache"
-export PATH="$PATH:$CIRCLE_WORKING_DIRECTORY/.bin"
+export WORK_DIR="$WORK_DIR/.cache/sccache"
+export SCCACHE_DIR="$WORK_DIR/.cache/sccache"
+export PATH="$PATH:$WORK_DIR/.bin"
 export RUSTC_WRAPPER="sccache"
 export RUST_MIN_STACK=8388608
 export RUSTFLAGS="-Clink-dead-code"
