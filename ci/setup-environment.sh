08ce4a2dfd861067a29d1ab4e8b6f22919f518f6
Daniil Ryzhkov <daniil.re@gmail.com>
2020-06-11 03:42:06 +0300

    atttempt to run without link-dead-code

--- ci/setup-environment.sh
+++ ci/setup-environment.sh
@@ -4,4 +4,3 @@ export SCCACHE_DIR="$CIRCLE_WORKING_DIRECTORY/.cache/sccache"
 export PATH="$PATH:$CIRCLE_WORKING_DIRECTORY/.bin"
 export RUSTC_WRAPPER="sccache"
 export RUST_MIN_STACK=8388608
-export RUSTFLAGS="-Clink-dead-code"
